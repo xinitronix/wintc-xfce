5cd77c3d9af7064a5d38c8694fbd03cdbb4072e1
Ваше Имя <you@example.com>
2025-02-07 16:16:20 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+cp -r fluxbox/fluxbox                        ~/.fluxbox
+cp -r fluxbox/config/fbmenugen               ~/.config/
+doas cp   fluxbox/config/fbmenugen/fbmenugen /usr/local/bin
+
+
+cp  -r  xfce/autostart                       ~/.config
+cp  -r  xfce/xfce                            ~/.config
+cp      xfce/gtkrc-2.0                       ~/.gtkrc-2.0
+cp      xfce/xinitrc                         ~/.xinitrc 
