5853c81479901296ddfcdc13c85c18a1184e6f04
Your Name <you@example.com>
2025-02-07 17:12:03 +0300

    переименовано: xfce/xfce4/desktop/accels.scm -> xfce/xfce/desktop/accels.scm 	переименовано: xfce/xfce4/desktop/icons.screen0.yaml -> xfce/xfce/desktop/icons.screen0.yaml 	переименовано: xfce/xfce4/panel/launcher-13/17389263751.desktop -> xfce/xfce/panel/launcher-13/17389263751.desktop 	переименовано: xfce/xfce4/panel/launcher-14/17389263752.desktop -> xfce/xfce/panel/launcher-14/17389263752.desktop 	переименовано: xfce/xfce4/panel/launcher-15/17389263753.desktop -> xfce/xfce/panel/launcher-15/17389263753.desktop 	переименовано: xfce/xfce4/panel/launcher-16/17389263754.desktop -> xfce/xfce/panel/launcher-16/17389263754.desktop 	переименовано: xfce/xfce4/xfconf/xfce-perchannel-xml/displays.xml -> xfce/xfce/xfconf/xfce-perchannel-xml/displays.xml 	переименовано: xfce/xfce4/xfconf/xfce-perchannel-xml/keyboards.xml -> xfce/xfce/xfconf/xfce-perchannel-xml/keyboards.xml 	переименовано: xfce/xfce4/xfconf/xfce-perchannel-xml/thunar.xml -> xfce/xfce/xfconf/xfce-perchannel-xml/thunar.xml 	переименовано: xfce/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml -> xfce/xfce/xfconf/xfce-perchannel-xml/xfce4-desktop.xml 	переименовано: xfce/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml -> xfce/xfce/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml

---
+++ git.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git add --all
+git add .
+git commit -n
+#git push https://github.com/xinitronix/mate.git
+git push ssh://git@github.com/xinitronix/wintc-xfce.git
